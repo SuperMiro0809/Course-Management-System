63712e3fcbbdacbb5cc1a1955da9ac09ebf9a0e1
Miroslav Balev <zevs8@abv.bg>
2025-05-17 22:47:55 +0300

    Add main cpp file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 
+#include "services/System.h"
+#include "utils/String.h"
+#include "commands/CommandFactory.h"
+#include "commands/Command.h"
+
 int main() {
-    std::cout << "Hello Word!" << std::endl;
+    using ::getline;
+    System system;
+
+    String input;
+    CommandFactory commandFactory;
+
+    std::cout << "Welcome to the Course Management System" << std::endl;
+
+    while (true) {
+        getline(std::cin, input);
+
+        if (input.empty()) continue;
+        if (input == "exit") break;
+
+        if (Command* cmd = commandFactory.create(input)) {
+            cmd->execute(system);
+            delete cmd;
+        } else {
+            std::cout << "Invalid command! Please try again" << std::endl;
+        }
+    }
 
     return 0;
 }
